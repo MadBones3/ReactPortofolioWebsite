b904bab9b08cf279176c03b35f4a0d70917745fb
Ashton <ashton@brandcandy.co.za>
2021-06-07 16:33:27 +0200

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,14 @@
 import React from 'react';
 //import components
 import AboutSection from '../components/AboutSection';
+import ServiceSection from '../components/ServicesSection';
 
 const AboutUs = () => {
     return(
+    <>
         <AboutSection/>
+        <ServiceSection/>
+    </>
     )
 }
 
