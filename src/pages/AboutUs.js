0c688e10f37c6275ae1699c52c413e5f7255682d
Ashton <ashton@brandcandy.co.za>
2021-06-07 16:11:45 +0200

    2. About Section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import components
+import AboutSection from '../components/AboutSection';
+
+const AboutUs = () => {
+    return(
+        <AboutSection/>
+    )
+}
+
+export default AboutUs;
