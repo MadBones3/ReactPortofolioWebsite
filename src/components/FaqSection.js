74c52522c9126ba8e031f61b6e8bd1a2602ccc63
Ashton <ashton@brandcandy.co.za>
2021-06-07 17:41:29 +0200

    7. Style FAQ section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import {Layout} from '../styles';
 
 const FaqSection = () => {
     return(
-        <div className="faq">
+        <FAQ>
             <h2>Any questions?</h2>
             <div className="question">
                 <h4>How do i start?</h4>
@@ -10,6 +13,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, praesentium?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily schedual</h4>
@@ -17,6 +21,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, praesentium?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Payment methods</h4>
@@ -24,6 +29,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, praesentium?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What products we offer</h4>
@@ -31,8 +37,37 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, praesentium?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </FAQ>
     )
 }
+
+// renaming layout to FAQ so can edit it specifically for this page
+const FAQ = styled(Layout)`
+    display: block;
+    span {
+        display: block;
+    }
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question P{
+        cursor: pointer;
+        padding: 3rem 0rem;
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p {
+            padding: 1rem 0rem;
+        }
+    }
+`;
 export default FaqSection;
