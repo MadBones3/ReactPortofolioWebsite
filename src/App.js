bc8d67b174db0a515d11df0dd31661ad152fa61f
Ashton <ashton@brandcandy.co.za>
2021-06-08 16:09:11 +0200

    11. Movie State and styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import GlobalStyles from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
 import ContactUs from './pages/ContactUs';
 import OurWork from './pages/OurWork';
+import MovieDetail from './pages/MovieDetail';
 import Nav from './components/Nav';
 //router
 import {Switch, Route} from 'react-router-dom';
@@ -19,8 +20,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs/>
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork/>
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail/>
         </Route>
         <Route path="/contact">
           <ContactUs/>
